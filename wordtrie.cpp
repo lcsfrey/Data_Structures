b6034a327244e36684c06d1b684447161a9f2034
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-07-30 11:32:29 -0700

    Qt Project File

---
+++ wordtrie.cpp
@@ -0,0 +1,6 @@
+#include "wordtrie.h"
+
+WordTrie::WordTrie()
+{
+
+}
---
+++ wordtrie.h
@@ -0,0 +1,11 @@
+#ifndef WORDTRIE_H
+#define WORDTRIE_H
+
+
+class WordTrie
+{
+public:
+    WordTrie();
+};
+
+#endif // WORDTRIE_H
