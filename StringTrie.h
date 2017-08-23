a50d00d4f3e376b3107882644e8f426cf710ebd8
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-08-23 14:53:43 -0700

    Moved StringTrie class definitions into separate cpp file

--- StringTrie.h
+++ StringTrie.h
@@ -31,32 +31,17 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <utility>
+
 struct StringTrieNode {
-    bool is_a_word = false;
-    int number_of_times_seen = 0;
-    char data;
-    StringTrieNode* parent = nullptr;
-    StringTrieNode** paths = new StringTrieNode*[200]();
     explicit StringTrieNode(char input_char) : data(input_char) {}
 
     // BUG: currently not deallocating memory correctly
-
-    ~StringTrieNode() {
-        std::vector<StringTrieNode*> all_paths;
-        StringTrieNode* current;
-        all_paths.push_back(this);
-        while (all_paths.size() > 0) {
-            current = all_paths.back();
-            all_paths.pop_back();
-            for (int i = 0; i < 200; i++) {
-                if (current->paths[i] != nullptr) {
-                    all_paths.push_back(current->paths[i]);
-                }
-            }
-            delete current->paths;
-            delete current;
-        }
-    }
+    ~StringTrieNode();
+    bool is_a_word = false;
+    char data;
+    StringTrieNode* parent = nullptr;
+    std::map<char, StringTrieNode*> m_paths;
 };
 
 class StringTrie {
@@ -64,215 +49,28 @@ class StringTrie {
     StringTrie();
     explicit StringTrie(const StringTrie &other_trie);
     ~StringTrie();
+
     void addWord(const std::string &word);
-    bool contains(const std::string &word);
+
     void remove(const std::string &word);
     void removeSubtrie(const std::string &word);
+
+    bool contains(const std::string &word);
+
     int getNumberTotalWords() const;
     int getNumberUniqueWords() const;
     int getLengthOfShortestWord() const;
     int getLengthOfLongestWord() const;
-    inline void printAll();
+
+    void printAll();
     void printAllWithPrefix(const std::string &word);
     void printAllHelper(StringTrieNode* current, std::string word);
-    std::map<char, StringTrieNode*> m_paths;
-    std::hash<char> m_hasher;
 
  private:
     StringTrieNode* head;
-
     int number_of_total_words = 0;
     int number_of_unique_words = 0;
-    const int alphabet_size = 200;
 };
 
-
-StringTrie::StringTrie() {
-    head = new StringTrieNode('\0');
-}
-
-StringTrie::~StringTrie() {
-    delete head;
-}
-
-void StringTrie::addWord(const std::string &word) {
-    StringTrieNode** current_list = head->paths;
-    StringTrieNode* current_node = head->paths[0];
-    const char* c_string_word = word.c_str();
-    int index_of_char = -1;
-    int size = word.length();
-    for (int i = 0; i < size; i++) {
-         char t_char = c_string_word[i];
-
-         //  index_of_char = m_hasher(c_string_word[i]);
-         //  if (m_paths[index_of_char] == nullptr){
-         //  StringTrieNode* t_tree_node;
-         //  t_tree_node = new StringTrieNode(c_string_word[i]);
-         //  std::pair<int, StringTrieNode*>  t_pair(index_of_char, t_tree_node);
-         //  m_paths.insert(t_pair);
-         //}
-        index_of_char = tolower(c_string_word[i]);
-        if (current_list[index_of_char] == nullptr) {
-            current_list[index_of_char] = new StringTrieNode(c_string_word[i]);
-            m_paths[t_char] = new StringTrieNode(c_string_word[i]);
-        }
-        current_node = current_list[index_of_char];
-        current_list = current_list[index_of_char]->paths;
-    }
-
-    if (!(current_node->is_a_word)) {
-        number_of_unique_words +=1;
-    }
-    current_node->is_a_word = true;
-    number_of_total_words += 1;
-}
-
-bool StringTrie::contains(const std::string &word) {
-    StringTrieNode** current_list = head->paths;
-    StringTrieNode* current_node = head->paths[0];
-    const char* c_string_word = word.c_str();
-    int index_of_char = -1;
-    int size = word.length();
-    for (int i = 0; i < size; i++) {
-        index_of_char = tolower(c_string_word[i]);
-        if (current_list[index_of_char] == nullptr) {
-            return false;
-        }
-        current_node = current_list[index_of_char];
-        current_list = current_list[index_of_char]->paths;
-    }
-    if (current_node->is_a_word) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-void StringTrie::remove(const std::string &word) {
-    StringTrieNode** current_list = head->paths;
-    StringTrieNode* current_node = head->paths[0];
-    std::vector<StringTrieNode*> node_stack;
-    const char* c_string_word = word.c_str();
-    int index_of_char = -1;
-    int size = word.length();
-    for (int i = 0; i < size; i++) {
-        index_of_char = tolower(c_string_word[i]);
-        if (current_list[index_of_char] == nullptr)
-            return;
-        node_stack.push_back(current_node);
-        current_node = current_list[index_of_char];
-        current_list = current_list[index_of_char]->paths;
-    }
-    if (current_node->is_a_word) {
-        current_node->is_a_word = false;
-        bool delete_node = true;
-        while (node_stack.size() > 0 && delete_node == true) {
-            for (int i = 0; i < alphabet_size; i++) {
-                if (current_node->paths[i] != nullptr) {
-                    delete_node = false;
-                }
-            }
-            if (delete_node == true) {
-                delete current_node;
-            }
-            current_node = node_stack.back();
-            node_stack.pop_back();
-        }
-    }
-}
-
-void StringTrie::removeSubtrie(const std::string &word) {
-    StringTrieNode** current_list = head->paths;
-    StringTrieNode* current_node = head->paths[0];
-    const char* c_string_word = word.c_str();
-    int index_of_char = -1;
-    int size = word.length();
-    for (int i = 0; i < size; i++) {
-        index_of_char = tolower(c_string_word[i]);
-        if (current_list[index_of_char] == nullptr) {
-            std::cout << "\nNo words with prefix: " << word << std::endl;
-            return;
-        }
-        current_node = current_list[index_of_char];
-        current_list = current_list[index_of_char]->paths;
-    }
-    delete current_node;
-}
-
-void StringTrie::printAll() {
-    printAllHelper(head, "");
-}
-
-// prints all words with a given prefix
-// input: prefix of words to print
-void StringTrie::printAllWithPrefix(const std::string &word) {
-    StringTrieNode** current_list = head->paths;
-    StringTrieNode* current_node = head->paths[0];
-    const char* c_string_word = word.c_str();
-    int index_of_char = -1;
-    int size = word.length();
-    for (int i = 0; i < size; i++) {
-        index_of_char = tolower(c_string_word[i]) - 97;
-        if (current_list[index_of_char] == nullptr) {
-            std::cout << "\nNo words with prefix: " << word << std::endl;
-            return;
-        }
-        current_node = current_list[index_of_char];
-        current_list = current_list[index_of_char]->paths;
-    }
-    printAllHelper(current_node, word.substr(0, word.size()-1));
-}
-
-// prints all words in subtree,
-// word is built up one character at a time with each rescursive call
-void StringTrie::printAllHelper(StringTrieNode* current, std::string word) {
-    if (current->is_a_word) {
-        std::cout << word + current->data << std::endl;
-    }
-    for (int i = 0; i < alphabet_size; i++) {
-        if (current->paths[i] != nullptr) {
-            printAllHelper(current->paths[i], word + current->data);
-        }
-    }
-}
-
-int StringTrie::getNumberTotalWords() const {
-    return number_of_total_words;
-}
-
-int StringTrie::getNumberUniqueWords() const {
-    return number_of_unique_words;
-}
-
-// returns the length of the shortest word in trie
-int StringTrie::getLengthOfShortestWord() const {
-    std::queue<StringTrieNode*>* possible_words;
-    possible_words = new std::queue<StringTrieNode*>();
-    StringTrieNode* current_node;
-    int min_length = 1;
-    int current_level_paths_left = 1;
-    int next_level_paths = 0;
-    possible_words->push(head);
-    while (possible_words->size() > 0) {
-        current_node = possible_words->front();
-        possible_words->pop();
-        for (int i = 0; i < alphabet_size; i++) {
-            if (current_node->paths[i] != nullptr) {
-                if (current_node->paths[i]->is_a_word) {
-                    return min_length;
-                }
-                next_level_paths++;
-                possible_words->push(current_node->paths[i]);
-            }
-        }
-        current_level_paths_left--;
-        if (current_level_paths_left == 0) {
-            min_length++;
-            current_level_paths_left = next_level_paths;
-            next_level_paths = 0;
-        }
-    }
-    return 0;
-}
 
 #endif  // STRINGTRIE_H_
