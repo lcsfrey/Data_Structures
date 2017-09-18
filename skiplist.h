f67573469cef8099411170026a295c2eb97d198b
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-09-17 23:20:02 -0700

    Added license, constructors, and methods
    Methods added:
       insert - insert new value into skiplist
       remove - remove value from skiplist
       contains - returns true if value is in skiplist
       print - prints all values in skiplist in order

--- skiplist.h
+++ skiplist.h
@@ -1,6 +1,32 @@
-#ifndef SKIPLIST_H
-#define SKIPLIST_H
+/************************************************************************************
+**                                                                                 **
+**  MIT License                                                                    **
+**                                                                                 **
+**  Copyright (c) 2017 Lucas Frey                                                  **
+**                                                                                 **
+**  Permission is hereby granted, free of charge, to any person obtaining          **
+**  a copy of this software and associated documentation files (the "Software"),   **
+**  to deal in the Software without restriction, including without limitation      **
+**  the rights to use, copy, modify, merge, publish, distribute, sublicense,       **
+**  and/or sell copies of the Software, and to permit persons to whom the          **
+**  Software is furnished to do so, subject to the following conditions:           **
+**                                                                                 **
+**  The above copyright notice and this permission notice shall be included        **
+**  in all copies or substantial portions of the Software.                         **
+**                                                                                 **
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS        **
+**  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    **
+**  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+**  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+**  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+**  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+**  SOFTWARE.                                                                      **
+**                                                                                 **
+************************************************************************************/
+#ifndef SKIPLIST_H_
+#define SKIPLIST_H_
 #include <vector>
+#include <random>
 
 ///////////////////////////////////////////////////////////////////////////////
 // DECLARATIONS
@@ -31,30 +57,48 @@ class SkipList {
      T back();
      inline int getLength() const;
      inline bool isEmpty() const;
+     void print() const;
 
  private:
      SkipListNode<T>* head;
      SkipListNode<T>* tail;
+     std::default_random_engine m_generator;
+     std::bernoulli_distribution m_distribution;
      int length;
 };
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // DEFINITIONS
 ///////////////////////////////////////////////////////////////////////////////
 
 template<class T>
-SkipList<T>::SkipList() {
+SkipListNode<T>::SkipListNode() {
+    next.push_back(nullptr);
+    prev.push_back(nullptr);
+}
+
+template<class T>
+SkipListNode<T>::SkipListNode(const T &value) {
+    data = value;
+    next.push_back(nullptr);
+    prev.push_back(nullptr);
+}
+
+template<class T>
+SkipList<T>::SkipList() : m_distribution(.5) {
     length = 0;
     tail = new SkipListNode<T>();
     head = new SkipListNode<T>();
+    head->next[0] = tail;
+    tail->prev[0] = head;
 }
 
 template<class T>
 SkipList<T>::~SkipList() {
-    SkipListNode<T>* current = head.at(0);
+    SkipListNode<T>* current = head;
+    SkipListNode<T>* next;
     while (current != tail) {
-        SkipListNode<T>* next = current->next.at(0);
+        next = current->next.front();
         delete current;
         current = next;
     }
@@ -64,8 +108,90 @@ SkipList<T>::~SkipList() {
 template<class T>
 void SkipList<T>::insert(const T &value) {
     int current_level = head->next.size() - 1;
-    SkipListNode<T>* current = head->next.back();
-    SkipListNode<T>* next = current->next.at(current_level);
+    std::vector<SkipListNode<T>*> jump_list(head->next.size());
+    SkipListNode<T>* current = head;
+    SkipListNode<T>* next = current->next.back();
+    while (current_level > 0) {
+        if(next != tail && next->data < value) {
+            current = next;
+            next = next->next.at(current_level);
+        } else {
+            jump_list[current_level] = current;
+            current_level--;
+            next = current->next.at(current_level);
+        }
+    }
+    while (next != tail && next->data < value) {
+        current = next;
+        next = next->next[0];
+    }
+    SkipListNode<T>* node_to_add = new SkipListNode<T>(value);
+    node_to_add->next[0] = next;
+    next->prev[0] = node_to_add;
+    node_to_add->prev[0] = current;
+    current->next[0] = node_to_add;
+
+    int size = jump_list.size();
+    int index = 1;
+    while (m_distribution(m_generator)) {
+        if (index < size) {
+            current = jump_list[index];
+            next = current->next[index];
+
+            node_to_add->next.push_back(next);
+            next->prev[index] = node_to_add;
+
+            node_to_add->prev.push_back(current);
+            current->next[index] = node_to_add;
+            index++;
+        } else {
+            head->next.push_back(node_to_add);
+            node_to_add->prev.push_back(head);
+            node_to_add->next.push_back(tail);
+            tail->prev.push_back(node_to_add);
+        }
+    }
+    length++;
+}
+
+template<class T>
+void SkipList<T>::remove(const T &value) {
+    int current_level = head->next.size() - 1;
+    SkipListNode<T>* current = head->next[0];
+    while (current != tail && current_level >= 0) {
+        if (current->data == value) {
+            SkipListNode<T>* node_to_delete = current;
+            for (int i = node_to_delete->next.size() - 1; i >= 0; i--) {
+                node_to_delete->prev[i]->next[i] = node_to_delete->next[i];
+                node_to_delete->next[i]->prev[i] = node_to_delete->prev[i];
+            }
+            delete node_to_delete;
+            length--;
+            return;
+        } else if (current->next[current_level] != tail &&
+                   current->next[current_level]->data <= value) {
+            current = current->next[current_level];
+        } else {
+            current_level--;
+        }
+    }
+}
+
+template<class T>
+bool SkipList<T>::contains(const T &value) const {
+    int current_level = head->next.size() - 1;
+    SkipListNode<T>* current = head;
+    do {
+        if (current->data == value) {
+            return true;
+        } else if (current->next[current_level] != tail &&
+                   current->next[current_level]->data <= value) {
+            current = current->next[current_level];
+        } else {
+            current_level--;
+        }
+    } while (current != tail && current_level >= 0);
+    return false;
 }
 
 template<class T>
@@ -78,8 +204,17 @@ bool SkipList<T>::isEmpty() const {
     return length == 0;
 }
 
+template<class T>
+void SkipList<T>::print() const {
+    SkipListNode<T>* current = head->next[0];
+    std::cout << "Head -> ";
+    while (current != tail->prev[0]) {
+        std::cout << current->data << " -> ";
+        current = current->next[0];
+    }
+    std::cout << current->data << " -> Tail" << std::endl;
+}
+
+#endif  // SKIPLIST_H_
 
 
-
-
-#endif // SKIPLIST_H
