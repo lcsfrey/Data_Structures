9e55e1af60eb7d23f32fce25e994100da9e88c2e
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-10-28 16:39:31 -0700

    Improved StringTrie and added SequenceTrie funcitions
    StringTrie functions:
       -buildStringFromFinalNode: returns the resulting string rather than storing it in reference argument
       -getNode: returns the last node of a string if it that node is the last node of a word
    
    StringRecord functions:
       -getOrderedWords: returns an ordered vector of pairs of strings and ints, where the string is the word, and the int is the number of times that string has been seen. The ordering the of vector is descending by their number of occurences
       -getOrderedOccurences: returns a vector of ints containing the number of occurences of each string in the record
    
    StringSequenceTrie functions:
       -addSequence: adds sequence of strings to SequenceTrie by calling addSequenceHelper
       -addSequenceHelper: helper function that adds sequences of strings to SequenceTrie
       -getNode: returns a pointer to the last node of a sequence representing the last string in that sequence. If the SequenceTrie does not contain that sequence, a nullptr is returned
       -loadTextFile: iterates through text file, adding every sequence of 5 words to the SequenceTrie

--- stringtrie.cpp
+++ stringtrie.cpp
@@ -32,9 +32,12 @@
 #include <utility>
 #include <string>
 #include <algorithm>
+#include <fstream>
+#include <chrono>
 
 StringTrieNode::StringTrieNode(char input_char)
   : data(input_char),
+    m_paths(),
     parent(nullptr),
     is_a_word(false) {}
 
@@ -58,6 +61,7 @@ StringTrie::~StringTrie() {
 }
 
 void StringTrie::addWord(const std::string &word) {
+    if (word == "" || word == " ") return;
     std::map<char, StringTrieNode*>* current_map = &head->m_paths;
     StringTrieNode* current_node = head;
     const char* c_string_word = word.c_str();
@@ -84,6 +88,7 @@ void StringTrie::addWord(const std::string &word) {
 
 bool StringTrie::contains(const std::string &word) {
     std::map<char, StringTrieNode*>* current_map = &head->m_paths;
+    if (current_map->size() == 0) return false;
     StringTrieNode* current_node = head;
     const char* c_string_word = word.c_str();
     char key_char = -1;
@@ -183,7 +188,7 @@ void StringTrie::printAllWithPrefix(const std::string &prefix) {
 
 // prints all words in subtree,
 // word is built up one character at a time with each rescursive call
-void StringTrie::printAllHelper(StringTrieNode* current, std::string word) {
+void StringTrie::printAllHelper(StringTrieNode* current, std::string word) const {
     if (current->is_a_word) {
         std::cout << word << std::endl;
     }
@@ -202,11 +207,9 @@ int StringTrie::getNumberUniqueWords() const {
 
 std::string StringTrie::getLongestWord() const {
     int length = 0;
-    std::string word = "";
     StringTrieNode* final_node = head;
     findLongestWord(head, 0, length, final_node);
-    buildStringFromFinalNode(final_node, word);
-    return word;
+    return buildStringFromFinalNode(final_node);
 }
 
 // returns the length of the shortest word in trie
@@ -238,12 +241,34 @@ int StringTrie::getLengthOfShortestWord() const {
 }
 
 // constructs string character by character
-void StringTrie::buildStringFromFinalNode(StringTrieNode* current_node,
-                                          std::string &word) const {
-    if (current_node->data != '\0') {
-        buildStringFromFinalNode(current_node->parent, word);
-        word += current_node->data;
-    }
+std::string StringTrie::buildStringFromFinalNode(const StringTrieNode* current_node) const {
+  if (current_node->data != '\0') {
+      return buildStringFromFinalNode(current_node->parent) + current_node->data;
+  } else {
+      return "";
+  }
+}
+
+
+StringTrieNode *StringTrie::getNode(const std::string &word) {
+  std::map<char, StringTrieNode*>* current_map = &head->m_paths;
+  StringTrieNode* current_node = head;
+  const char* c_string_word = word.c_str();
+  char key_char = -1;
+  int size = word.length();
+  for (int i = 0; i < size; i++) {
+      key_char = tolower(c_string_word[i]);
+      if (current_map->find(key_char) == current_map->end()) {
+          return nullptr;
+      }
+      current_node = current_map->at(key_char);
+      current_map = &current_node->m_paths;
+  }
+  if (current_node->is_a_word) {
+      return current_node;
+  } else {
+      return nullptr;
+  }
 }
 
 // returns longest word in trie
@@ -262,9 +287,9 @@ void StringTrie::findLongestWord(StringTrieNode* current_node, int current_lengt
 int StringTrie::getLengthOfLongestWord() const {
     int max_length = 0;
     StringTrieNode* final_node = head;
-    std::string word = "";
+
     findLongestWord(head, 0, max_length, final_node);
-    buildStringFromFinalNode(final_node, word);
+    std::string word = buildStringFromFinalNode(final_node);
     std::cout << word << std::endl;
     return max_length;
 }
@@ -273,28 +298,177 @@ int StringTrie::getLengthOfLongestWord() const {
 void StringRecord::addWord(StringTrieNode *current_node) {
     auto it = m_record.find(current_node);
     if (it == m_record.end()) {
-        m_record[current_node] == 1;
+        m_record[current_node] = 1;
     } else {
         it->second += 1;
     }
 }
 
-std::vector<std::pair<std::string, int>> StringRecord::getOrderedWords(const StringTrie *trie) {
-  std::vector<std::pair<std::string, int>> words;
-  int word_occurences = 0;
-  std::string word = "";
-  for (const auto &pair : m_record) {
-      if (pair.second < 40) continue;
-      trie->buildStringFromFinalNode(pair.first, word);
-      word_occurences = pair.second;
-      words.push_back(std::pair<std::string, int>(std::move(word), word_occurences));
+
+std::vector<std::pair<std::string, int>> StringRecord::getOrderedWords(const StringTrie *trie,
+                                                                       const int lower_limit,
+                                                                       const int upper_limit) {
+    std::vector<std::pair<std::string, int>> words;
+    int word_occurences = 0;
+    std::string word = "";
+
+    // add pair to vector if seen more than 40 times
+    for (const std::pair<StringTrieNode*, int> &pair : m_record) {
+        if (pair.second < lower_limit || pair.second > upper_limit) continue;
+
+        word = trie->buildStringFromFinalNode(pair.first);
+        word_occurences = pair.second;
+        words.push_back(std::pair<std::string, int>(std::move(word), word_occurences));
+    }
+
+    // sort vector by the second value in pair
+    std::sort(words.begin(), words.end(),
+              [](const std::pair<std::string, int> &left,
+                 const std::pair<std::string, int> &right) {
+        return left.second > right.second;
+    });
+    return words;
+}
+
+std::vector<int> StringRecord::getOrderedOccurences() {
+    std::vector<int> occurences;
+    for (const auto &pair : m_record) {
+        occurences.push_back(pair.second);
+    }
+    std::sort(occurences.begin(), occurences.end(), std::greater<int>());
+    return occurences;
+}
+
+StringSequenceTrie::StringSequenceTrie() : m_total_words(0),
+                                           m_trie(new StringTrie()),
+                                           head(new StringSequenceTrieNode(m_trie->head, nullptr)) {}
+
+void StringSequenceTrie::addSequence(const std::string &sequence) {
+    addSequenceHelper(sequence, 0, true);
+    // std::cout << "Adding to Sequence Trie: " << sequence << std::endl;
+}
+
+void StringSequenceTrie::addSequenceHelper(const std::string &sequence,
+                                             short starting_pos,
+                                             bool add_word_to_trie) {
+
+    // verify we have a sequence to look at
+    std::size_t size = sequence.size();
+
+    std::size_t space_index = -1;
+
+    std::string str = "";
+    StringTrieNode* current_trie_node = nullptr;
+    StringSequenceTrieNode* current_sequence_node = head;
+    StringSequenceTrieNode* parent = head;
+    do {
+        space_index = sequence.find(" ", starting_pos);
+        // check and verify there is a space in the string, else return
+        if (space_index < size) {
+            str = sequence.substr(starting_pos, space_index - starting_pos);
+        } else {
+            str = sequence.substr(starting_pos, size - starting_pos);
+        }
+
+        if (add_word_to_trie) {
+            m_trie->addWord(str);
+        }
+        current_trie_node = m_trie->getNode(str);
+
+        if (current_sequence_node->m_next_word.find(current_trie_node) == current_sequence_node->m_next_word.end()) {
+            current_sequence_node->m_next_word[current_trie_node] = new StringSequenceTrieNode(current_trie_node, parent);
+            current_sequence_node = current_sequence_node->m_next_word[current_trie_node];
+        } else {
+            current_sequence_node = current_sequence_node->m_next_word[current_trie_node];
+            current_sequence_node->addOneTimeSeen();
+        }
+
+        parent = current_sequence_node;
+
+        if (space_index >= size) return;
+        // add sequence of words starting at current_pos
+        addSequenceHelper(sequence, (int)space_index + 1, add_word_to_trie);
+        add_word_to_trie = false;
+        starting_pos = space_index + 1;
+     } while (space_index < size);
+}
+
+const StringSequenceTrieNode *StringSequenceTrie::getNode(std::string &sequence)
+{
+  // verify we have a sequence to look at
+  std::size_t size = sequence.size();
+
+  // find the next space in the sentence
+  std::size_t space_index = sequence.find(" ");
+
+  std::string str = "";
+  StringTrieNode* current_trie_node = nullptr;
+  int current_pos = 0;
+  StringSequenceTrieNode* current_record_node = head;
+
+  do {
+      // check and verify there is a space in the string, else return
+      if (space_index < size) {
+          str = sequence.substr(current_pos, space_index - current_pos);
+      } else {
+          str = sequence.substr(current_pos, size - current_pos);
+      }
+
+      // check if the word is in m_trie
+      // if it is not, then it wouldn't be in the sequence record
+      current_trie_node = m_trie->getNode(str);
+      if (current_trie_node == nullptr) {
+          return nullptr;
+      }
+
+      if (current_record_node->m_next_word.find(current_trie_node) == current_record_node->m_next_word.end()) {
+          return nullptr;
+      } else {
+          current_record_node = current_record_node->m_next_word[current_trie_node];
+      }
+  } while (space_index < size);
+  return current_record_node;
+}
+
+
+void StringSequenceTrie::loadTextFile(std::string file_name) {
+  if (file_name == "") file_name = "GreatExpectations.txt";
+  std::ifstream my_file;
+  my_file.open(file_name);
+  if(!my_file.is_open()){
+      std::cout << "File in trieTest() didn't open!\n";
+  }
+  std::cout << "Now Loading " << file_name << "...\n";
+
+  char* c;
+  std::string temp_word;
+  std::clock_t start = clock();
+  std::string sequence = "";
+
+  int count = 1;
+  my_file >> sequence;
+  // Add 5 words in queue together and add sequence to StringSequenceTrie
+  while (!my_file.eof() && count < 5){
+      my_file >> temp_word;
+      if(temp_word != "----------------------------------------"){
+          sequence += " " + temp_word;
+          count++;
+      }
   }
 
-  std::sort(words.begin(), words.end(),
-            [](const std::pair<std::string, int> &left,
-               const std::pair<std::string, int> &right) {
-      return left.second > right.second;
-  });
-  return words;
+  addSequence(sequence);
+
+  while (!my_file.eof()) {
+      my_file >> temp_word;
+      if(temp_word != "----------------------------------------"){
+          int space = sequence.find(" ");
+          sequence.erase(0, space + 1);
+          sequence.append(" " + temp_word);
+          addSequence(sequence);
+      }
+  }
+  std::clock_t duration = clock() - start;
+  std::cout << "Time taken: " << duration / (double)CLOCKS_PER_SEC << std::endl;
+  my_file.close();
 }
 
