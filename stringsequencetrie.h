0052058adfb49026cd0839e8febaafb7b13b28b8
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-11-02 22:12:30 -0700

    Added order functions and changed sequence adding
    Functions:
    
    -printOrderedWords:
    
          Prints all next potential words sorted by the times they've been seen
    
    -getOrderedWords:
    
          Adds most likely next strings starting after specified starting
          sequence(if no sequence is given, the algorithm begins at the
          head node) as StringSequenceTrie pointer to a vector, sorts the
          vector by the times they've been seen and returns the vector.
    
          Supports getting strings of a specific range of lengths,
          occurences, and branching factor that specifies how many strings
          to add at each branch to a next potential string
    
    -getOrderedWordsHelper:
    
          Recursive function that takes a StringSequenceNode pointer
          a window size,and a branching factor, gets each
          StringSequenceNode pointer from m_next_word map to a vector,
          sorts the vector by the times it has been seen, recursively
          calls itself window_size times on the next highest seen
          StringSequenceTrieNodes in the vector, then moves those pointers
          to the vector containing highest seen pointers at each branch
    
    changed space searching in addSequenceHelper
    
    added window_size argument to loadTextFile function as well

--- stringsequencetrie.h
+++ stringsequencetrie.h
@@ -1,8 +1,35 @@
+/************************************************************************************
+**                                                                                 **
+**  MIT License                                                                    **
+**                                                                                 **
+**  Copyright (c) 2017 Lucas Frey                                                  **
+**                                                                                 **
+**  Permission is hereby granted, free of charge, to any person obtaining          **
+**  a copy of this software and associated documentation files (the "Software"),   **
+**  to deal in the Software without restriction, including without limitation      **
+**  the rights to use, copy, modify, merge, publish, distribute, sublicense,       **
+**  and/or sell copies of the Software, and to permit persons to whom the          **
+**  Software is furnished to do so, subject to the following conditions:           **
+**                                                                                 **
+**  The above copyright notice and this permission notice shall be included        **
+**  in all copies or substantial portions of the Software.                         **
+**                                                                                 **
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS        **
+**  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    **
+**  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    **
+**  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         **
+**  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  **
+**  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  **
+**  SOFTWARE.                                                                      **
+**                                                                                 **
+************************************************************************************/
+
 #ifndef STRINGSEQUENCETRIE_H
 #define STRINGSEQUENCETRIE_H
 
 
 #include "stringtrie.h"
+
 
 class StringSequenceTrieNode {
  public:
@@ -41,18 +68,38 @@ class StringSequenceTrie {
     // add sequence of strings from vector to record
     void addSequence(std::vector<std::string> &sequence, int window_size = 5);
 
+    std::vector<StringSequenceTrieNode*> getOrderedWords(const std::string sequence = "",
+                                                         int sequence_length_upper_limit = INT32_MAX,
+                                                         int sequence_length_lower_limit = 1,
+                                                         int branching_factor = 5);
+
+    void printOrderedWords(const std::string sequence = "",
+                           int sequence_length_upper_limit = INT32_MAX,
+                           int sequence_length_lower_limit = 1,
+                           int branching_factor = 5,
+                           int frequency_upper_limit = INT32_MAX,
+                           int frequency_lower_limit = 0);
+
     // returns a sequence of strings one string at a time from the last node;
     std::string buildSequenceFromFinalNode(const StringSequenceTrieNode *current) const;
 
-    void loadTextFile(std::string file_name = "");
+    void loadTextFile(std::string file_name = "", int window_size = 5);
 
  protected:
     // returns a node pointing to the last node in the sequence
     StringSequenceTrieNode* getNode(const std::string &sequence) const;
 
 
-    void addSequenceHelper(const std::string &sequence, int starting_pos = 0,
-                           bool add_word_to_trie = true);
+    void addSequenceHelper(const std::string &sequence, StringSequenceTrieNode *current_node,
+                           int starting_pos = 0);
+
+
+    void getOrderedWordsHelper(const StringSequenceTrieNode* current_node,
+                               std::vector<StringSequenceTrieNode *> &sequences,
+                               const int &sequence_length_upper_limit = INT32_MAX,
+                               const int &sequence_length_lower_limit = 1,
+                               int current_sequence_length = 0,
+                               int branching_factor = 5);
 
  private:
     // contains dictionary of words that have been used
@@ -62,4 +109,6 @@ class StringSequenceTrie {
 
     int m_total_words;
 };
+
+
 #endif // STRINGSEQUENCETRIE_H
